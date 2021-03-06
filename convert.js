8d7e58fcb9765e80edcabe8cb1e7d257c45b4680
FlorianElke <florian@florian-elke.de>
2021-03-06 15:18:59 +0100

    add files

---
+++ convert.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+
+
+function readfiles() {
+	let filenames = [];
+	const testFolder = './mp4/';
+
+	fs.readdir(testFolder, (err, files) => {
+		files.forEach(file => {
+			filenames.push('./mp4/'+file);
+		});
+		console.log(filenames);
+		tomp3(filenames);
+	});
+}
+
+function tomp3(filename) {
+	filename.forEach((element)=>{
+		console.log('\nConverting File to mp3...');
+		const songnamenew = element.substring(6, element.length -4);
+		ffmpeg({source:element})
+			.toFormat('mp3')
+			.saveToFile('./mp3/'+songnamenew+ '.mp3', ()=>{
+				console.log('done');
+				
+			} );
+	});
+}
+readfiles();
---
+++ main.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const fs   = require('fs');
+const youtubedl = require('youtube-dl');
+const tomp3 = require('./convert');
+
+
+let filename = [];
+
+function start() {
+	ask((answer)=>{
+		const url = answer;
+		playlist(url);
+	});
+}
+
+function ask(callback) {
+	const readline = require('readline').createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+	readline.question('Please Enter Playlist URL:\n', (answer) => {
+		readline.close();
+		callback(answer); 
+
+	});
+}
+function playlist(url) {
+	'use strict';
+	const video = youtubedl(url);
+	
+	video.on('error', (err) =>{
+		console.log('error 2:', err);
+	});
+	
+	let size = 0;
+	video.on('info', function(info) {
+		size = info.size;
+		let output = path.join(__dirname + '/mp4/', info._filename);
+		video.pipe(fs.createWriteStream(output));
+		console.log('\nDownload started');
+		console.log('filename: ' + info._filename);
+		console.log('size: ' + info.size);
+		filename.push('./mp4/'+info._filename);
+	});
+	
+	let pos = 0;
+	video.on('data', function data(chunk) {
+		pos += chunk.length;
+		if (size) {
+			let percent = (pos / size * 100).toFixed(2);
+			process.stdout.cursorTo(0);
+			process.stdout.clearLine(1);
+			process.stdout.write(percent + '%');
+		}
+	});
+
+	video.on('next', playlist);
+
+}
+
+
+start();
